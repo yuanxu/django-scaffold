40576c1294c03632600190d140ecc14211f40873
YuanXu <ankh2008@hotmail.com>
2015-06-13 10:36:19 +0800

    增加默认的url

---
+++ scaffold_toolkit/urls.py
@@ -0,0 +1,6 @@
+from django.conf.urls import url
+from scaffold_toolkit.views import kindeditor_upload_file, tag_suggestion
+
+urlpatterns = [url(r'^kind_upload/$', kindeditor_upload_file, name='kind_upload'),
+               url(r'^tag_suggestion/$', tag_suggestion, name='tag_suggestion'),
+               ]
