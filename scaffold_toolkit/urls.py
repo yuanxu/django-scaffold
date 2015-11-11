44eb12ef64e651582ab3397ed40a668f39d126df
YuanXu <ankh2008@hotmail.com>
2015-11-11 15:27:15 +0800

    修复AutoTagField缺失tag_suggestion 错误

--- scaffold_toolkit/urls.py
+++ scaffold_toolkit/urls.py
@@ -1,9 +1,10 @@
 # coding=utf-8
 from django.conf.urls import url
 from django.views.generic import TemplateView
-from scaffold_toolkit.views import kindeditor_upload_file
+from scaffold_toolkit.views import kindeditor_upload_file, tag_suggestion
 
 urlpatterns = [url(r'^kind_upload/$', kindeditor_upload_file, name='kind_upload'),
                url(r'^dialog-success/$', TemplateView.as_view(template_name='common/dialog_success.html'),
                    name='dialog_success'),
+               url(r'^tag_suggestion/$', tag_suggestion, name='tag_suggestion'),
                ]
--- scaffold_toolkit/views.py
+++ scaffold_toolkit/views.py
@@ -8,11 +8,12 @@ from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_http_methods
 import os
 from django.conf import settings
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.views.generic import View, FormView
 from scaffold_toolkit.utilities.misc import get_form_error_message
 import shortuuid
 from braces.views import OrderableListMixin as olm
+from tagging.models import Tag
 
 
 class AjaxFormView(JSONResponseMixin, AjaxResponseMixin, FormView):
@@ -139,3 +140,19 @@ def kindeditor_upload_file(request):
     return HttpResponse(json.dumps(
         {'error': 0, 'url': url_name}
     ))
+
+
+MAX_RESULT = getattr(settings, 'MAX_RESULT', 10)
+
+
+def tag_suggestion(request):
+    """
+    tag建议
+    @param request:
+    @return:
+    """
+    term = request.GET.get('term')
+
+    tags = [{'id': tag.name, 'text': tag.name}
+            for tag in Tag.objects.filter(name__icontains=term)[:MAX_RESULT]]
+    return JsonResponse(tags, safe=False)
